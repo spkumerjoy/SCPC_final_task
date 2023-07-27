6cadc23b422ced3c4921dd47c30bf0656c29b709
spkumerjoy <joyskydd@gmail.com>
2023-07-27 20:12:06 +0600

    Task 6 is complete

--- SCPC_final_task.js
+++ SCPC_final_task.js
@@ -72,3 +72,27 @@ const num2 = 3;
 const operator = "+";
 const result = calculate(num1, num2, operator);
 console.log(result);
+
+// Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+function generateRandomPassword(length) {
+    const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()_-+=[]{}|;:,.<>?";
+
+    const allChars =
+        uppercaseLetters + lowercaseLetters + numbers + specialChars;
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars.charAt(randomIndex);
+    }
+
+    return password;
+}
+
+const passwordLength = 12;
+const randomPassword = generateRandomPassword(passwordLength);
+console.log(randomPassword);
