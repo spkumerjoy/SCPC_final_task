693d972c9515cd3923611d791a49fb9c70af0e3f
spkumerjoy <joyskydd@gmail.com>
2023-07-27 17:31:27 +0600

    1st Task is complete

--- SCPC_final_task.js
+++ SCPC_final_task.js
@@ -1,1 +1,13 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+function reverseString(str) {
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+const inputString = "hello world";
+const outputString = reverseString(inputString);
+console.log(outputString);
