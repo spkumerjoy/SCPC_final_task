8a553088532d603ead77fcfc27cc368a1485ded0
spkumerjoy <joyskydd@gmail.com>
2023-07-27 17:32:37 +0600

    2nd Task is complete

--- SCPC_final_task.js
+++ SCPC_final_task.js
@@ -11,3 +11,19 @@ function reverseString(str) {
 const inputString = "hello world";
 const outputString = reverseString(inputString);
 console.log(outputString);
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+
+function sumPositiveNumbers(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        if (num > 0) {
+            sum += num;
+        }
+    }
+    return sum;
+}
+
+const inputArray = [2, -5, 10, -3, 7];
+const outputSum = sumPositiveNumbers(inputArray);
+console.log(outputSum);
